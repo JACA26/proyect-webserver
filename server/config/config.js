dbd019f1046f0184d28d226dad12a0caba8f4b30
Alexander <alexdead2118@gmail.com>
2020-12-01 19:01:14 -0500

    fixed mongodb errors

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://JACA:8yKwRzIUP0MeNqEo@cluster0.zxld2.mongodb.net/cafe?retryWrites=true&w=majority';
 }
 process.env.URLDB = urlDB;
